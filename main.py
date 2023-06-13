eb94994e73dac3ff55d8f00953555c01189fa177
Rassena <gadzinski.michal@gmail.com>
2023-06-13 18:38:54 +0200

    some really big issues

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import numpy as np
 import pandas as pd
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import Normalizer, StandardScaler
+
+names = ["ID", "RI", "NA2O", "MGO", "AL2O3", "SIO2", "K2O", "CAO", "BAO", "FE203", "TYPE"]
 df = pd.read_csv(
     "data/glass.data",
-    names=["ID", "RI", "NA2O", "MGO", "AL2O3", "SIO2", "K2O", "CAO", "BAO", "FE203", "TYPE"]
+    names=names
 )
 
 print(df)
@@ -16,15 +18,20 @@ metrics = ("accuracy", "f1_weighted", "recall_weighted")
 
 scaler = StandardScaler()
 normalizer = Normalizer()
+pca1 = PCA(n_components=1)
+pca2 = PCA(n_components=2)
 
-scaled_data = scaler.fit_transform(df)
-normalized_data = normalizer.transform(df)
-
-X = df.iloc[:, :-1].to_numpy()
-y = df.iloc[:, -1].to_numpy()
-
+scaled_data = pd.DataFrame(scaler.fit_transform(df),columns=names)
+normalized_data = pd.DataFrame(normalizer.transform(df),columns=names)
+pca1_data = pd.DataFrame(pca1.fit_transform(df),columns=['PCA_1'])
+pca2_data = pd.DataFrame(pca2.fit_transform(df),columns=['PCA_1','PCA_2'])
 
 
+data_sets = {
+    "df": df,
+    "pca1_data":pca1_data,
+    "pca2_data":pca2_data
+}
 
 classifiers = {
     "GaussianNB": GaussianNB(),
@@ -34,8 +41,13 @@ classifiers = {
 }
 
 for classifier_name, classifier in classifiers.items():
+    print("\n", classifier_name)
     for metric in metrics:
-        scores = cross_val_score(classifier, X, y, cv=3, scoring=metric)
-        print(classifier_name, metric, np.mean(scores))
+        for data_name, data in data_sets.items():
+            X = data.iloc[:, :-1].to_numpy()
+            y = data.iloc[:, -1].to_numpy()
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+            # scores = cross_val_score(classifier, X, y, cv=3, scoring=metric)
+            print(data_name, classifier_name, metric, np.mean(scores))
 
 
